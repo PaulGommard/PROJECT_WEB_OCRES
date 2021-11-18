be9c7407f81fcb56598a0d9390165ed3eb5f727f
Paul_Gommard <57675875+PaulGommard@users.noreply.github.com>
2021-11-18 16:41:47 +0100

    Display all most popular films using an API

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,73 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const API_KEY = 'api_key=aa090ed40895efe0d080b831bf9420ae';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?'+API_KEY;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            isLoaded: false,
+        }
+    }
+
+    componentDidMount() {
+        fetch(API_URL)
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    isLoaded: true,
+                    items: json,
+                })
+                console.log(json);
+            });
+            
+    }
+    render() {
+
+        var { isLoaded, items } = this.state;
+
+        if (!isLoaded) {
+            return <div>Loading...</div>
+        }
+
+        else {
+            return (
+                <div className="App">  
+                    <header>
+                        <form id="form">
+                            <input type="text" placeholder="search" id="search" class="search" />
+                        </form>
+                    </header>  
+
+                    <main id="main">
+                        {items.results.map(item => (
+                            <div class="movie"> 
+                                <img src={"https://image.tmdb.org/t/p/original/" + item.poster_path} alt="Image" />
+                                <div class="movie-info">
+                                    <h3>{item.title}</h3>
+                                    <span class="green">{item.vote_average}</span>
+                                </div>
+
+                                <div class="overview">
+                                    {item.overview}
+                                </div>
+                            </div>
+                        ))}
+                    </main>
+                </div>
+            );
+        }
+        
+    }
+    
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
